91b347ca337914eb1a0e18f67ad19ba28b2dbd5b
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2022-07-21 17:25:31 +0545

    Modified main.cpp after successful open of credits window through main menu

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include "Menu.hpp"
+#include "Credits.hpp"
+
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
 	//	Create window
-	sf::RenderWindow window;
-	window.create(sf::VideoMode(800, 600), "Call Break");
+	sf::RenderWindow MENU_WINDOW, CREDITS_WINDOW;
+	MENU_WINDOW.create(sf::VideoMode(800, 600), "Call Break");
 
-	//	Menu stuff
-	Menu menu(window.getSize().x, window.getSize().y);
-	
-	//	Loaded texture 
+	//	Main Menu window stuff
+	Menu menu(MENU_WINDOW.getSize().x, MENU_WINDOW.getSize().y);
+
+	//	Credits window stuff
+	Credits creditsObj(MENU_WINDOW.getSize().x);
+
+	//	Load texture for background image and load img
 	sf::Texture texture;
 	if (!texture.loadFromFile("src/Images/card_background_menu.jpg"))
 	{
@@ -20,15 +25,15 @@ int main()
 	sf::Sprite menuBackground(texture);
 
 	//	Game loop
-	while (window.isOpen())
+	while (MENU_WINDOW.isOpen())
 	{
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (MENU_WINDOW.pollEvent(event))
 		{
-				if (event.type == sf::Event::Closed)
-				{
-					window.close();
-				}
+			if (event.type == sf::Event::Closed)
+			{
+				MENU_WINDOW.close();
+			}
 			if (event.type == sf::Event::KeyPressed)
 			{
 				//	Up arrow key pressed
@@ -48,37 +53,70 @@ int main()
 
 				if (event.key.code == sf::Keyboard::Return)
 				{
-					switch (menu.getPressedLabel()){
-						case 0:
-							//	Main Game loop here
-							std::cout << "Play pressed";
-							break;
-						case 1:
-							//	Show credits
-							std::cout << "Credits pressed";
-							break;
-						case 2:
-							//	Exit from game
-							window.close();
-							break;
-						default:
-							std::cout << "Not valid";
-							break;
+					if (menu.getPressedLabel() == 0)
+					{
+						//	Main Game loop here
+						std::cout << "Play pressed";
+					}
+					if (menu.getPressedLabel() == 1)
+					{
+						//	Show credits when 'Enter' pressed in Credits
+
+						//	First close menu window
+						MENU_WINDOW.close();
+
+						//	Create new Credits window
+						CREDITS_WINDOW.create(sf::VideoMode(800, 600), "Call Break");
+
+						//	Load texture for credits background
+						sf::Texture texture;
+						if (!texture.loadFromFile("src/Images/creditsBackground.jpg"))
+						{
+							std::cout << "Error loading menu background image.";
+						}
+						sf::Sprite creditsBackground(texture);
+
+						//	Game loop
+						while (CREDITS_WINDOW.isOpen())
+						{
+							sf::Event event;
+							while (CREDITS_WINDOW.pollEvent(event))
+							{
+								if (event.type == sf::Event::Closed)
+								{
+									CREDITS_WINDOW.close();
+								}
+							}
+
+							CREDITS_WINDOW.draw(creditsBackground);
+
+							//	Render text
+							creditsObj.renderCredits(CREDITS_WINDOW);
+
+							//	end of current frame
+							CREDITS_WINDOW.display();
+						}
+
+						if (menu.getPressedLabel() == 2)
+						{
+							MENU_WINDOW.close();
+						}
 					}
 				}
+			}
+		}
 
-		}
-		}
+		MENU_WINDOW.clear();
+
+		//	Draw menu background
+		MENU_WINDOW.draw(menuBackground);
 		
-		window.clear();
-		window.draw(menuBackground);
-		//	Draw menu
-		menu.draw(window);
+		//	Member function of menu obj to draw menu
+		menu.draw(MENU_WINDOW);
 
 		//	end of current frame
-		
-		window.display();
 
+		MENU_WINDOW.display();
 	}
 	return 0;
 }
